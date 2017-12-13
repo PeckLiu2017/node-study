41bcee1c1eb19732f2fd5ae0b07b84c4c4a1f2a4
peck liu <xiaoyang20170204@gmail.com>
2017-12-13 13:52:41 +0800

    app3.js parse post type request

---
+++ express-demo/app3.js
@@ -0,0 +1,29 @@
+var express    = require('express');
+var app        = express();
+
+// 处理POST、PUT、DELETE等请求
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 端口和路由
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.send('<h1>Hello World</h1>');
+});
+
+router.get('/:name', function(req, res) {
+  res.send('<h1>Hello ' + req.params.name + '</h1>');
+});
+
+router.post('/', function (req, res) {
+  var name = req.body.name;
+  res.json({message: 'Hello ' + name});
+});
+
+app.use('/home', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
